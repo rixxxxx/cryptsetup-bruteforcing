d7c5cdbf3d3628ac1b183d84b40111ea14c9a954
Karlo Bartha <rixxxxx@users.noreply.github.com>
2021-03-14 18:14:16 +0100

    Update password-tester.sh

--- password-tester.sh
+++ password-tester.sh
@@ -4,6 +4,6 @@ n=1
 while read line; do
 # reading each line
 echo line number $n:
-echo $line | sudo cryptsetup open /dev/sda2 USB -
+echo $line | sudo cryptsetup open /dev/sda2 lukssystem -
 n=$((n+1))
 done < $filename
